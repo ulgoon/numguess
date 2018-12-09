6aa7bb5bf95ff4dd7f7022c3b965ca2d52c1fc54
kwonohdong <odong831@gmail.com>
2018-12-09 09:56:50 +0900

    Edit numguess.py
    Complete source code for numguess problem!

--- numguess.py
+++ numguess.py
@@ -6,3 +6,13 @@ import random
 # print answer
 answer = random.randint(1,100)
 print(answer)
+
+# user try count
+trial_cnt = 5
+
+while trial_cnt > 0:
+    # user input answer
+    user_answer = int(input('Enter your answer: '))
+
+    trial_cnt -= 1
+
