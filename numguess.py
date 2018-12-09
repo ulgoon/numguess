3e7caf0d30a85a7204f6b6126b7f5d4ed346db49
choiic <choic83@hotmail.com>
2018-12-09 09:10:25 +0900

    edit numguess.py input user number if input number is not valid, input number again compare with input number and the answer

--- numguess.py
+++ numguess.py
@@ -5,4 +5,29 @@ import random
 # assign answer with randint between 1 to 100
 # print answer
 answer = random.randint(1,100)
-print(answer)
+# print(answer)
+
+# by choiic
+# assign user_num with input()
+# check user_num is between 1 to 100
+# if user_num is not valid, print some message and input user_num again.
+# if user_num is not same with answer, print some message and input user_num again. 
+while True:
+    try:
+        user_num = int(input("input your guess number(integer type, 1~100): "))
+        if (user_num < 1 or user_num > 100) == True:
+            print("Input number in range 1~100")
+            continue
+        else:
+            if user_num == answer:
+                print("Congratulations!! You win!!")
+                break
+            elif user_num > answer:
+                print("Think smaller number again!, Your number is greater than the answer!")
+                continue
+            else:
+                print("Think bigger number again!, Your number is smaller than the answer!")
+                continue
+    except:
+        print("Input Integer number in range 1~100")
+        continue
